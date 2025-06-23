ee617350e2a8feb5df446d4c7b8553e115376e38
Serhii.Tsybulskyi <c4r0n0s@gmail.com>
2025-06-23 13:07:29 +0300

    update layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,13 @@
-import { Metadata } from "next";
+import { Metadata } from 'next';
 
 export const metadata: Metadata = {
-  title: "Nextra 4",
-  description: "Nextra 4 is here.",
+  title: 'Nextra 4',
+  description: 'Nextra 4 is here.',
 };
 export default function Home() {
   return (
-    <div className="container flex items-center justify-center">
-      <h1
-        style={{
-          textAlign: "center",
-          fontSize: 64,
-          margin: "25vh 0",
-          fontWeight: "bold",
-        }}
-      >
-        Home page
-      </h1>
+    <div className='w-full flex flex-1 items-center justify-center'>
+      <h1 className='text-4xl font-bold'>Home page</h1>
     </div>
   );
 }
