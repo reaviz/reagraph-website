b4e10206d24ed3bc0afd0f7a0bfa2cc2ce6c4ad7
Serhii.Tsybulskyi <c4r0n0s@gmail.com>
2025-07-01 14:33:20 +0300

    fix svg with layout and add getting started section

--- src/components/blocks/getting-started.tsx
+++ src/components/blocks/getting-started.tsx
@@ -1,9 +1,43 @@
 'use client';
 import { FC } from 'react';
-import { cn, Stack } from 'reablocks';
+import { Button, cn, Stack } from 'reablocks';
 
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
 import { vscDarkPlus } from 'react-syntax-highlighter/dist/cjs/styles/prism';
+import Link from 'next/link';
+
+const BASIC_CODE_SAMPLE = `<GraphCanvas
+  nodes={[
+    { id: '1', label: 'Node 1'},
+    { id: '2', label: 'Node 2' }
+  ]}
+  edges={[
+    { id: '1-2', source: '1', target: '2', label: 'Edge 1-2' },
+    { id: '2-1', source: '2', target: '1', label: 'Edge 2-1' }
+  ]}
+/>
+`;
+
+const CUSTOM_THEME_CODE_SAMPLE = `const App = () => (
+  <GraphCanvas
+    theme={{
+      ...lightTheme,
+      node: { ...lightTheme.node, color: '#000' }
+    }}
+    renderNode={({ size, color }) => (
+      <group>
+        <mesh>
+          <torusKnotGeometry attach="geometry" args={[size, 1.25, 50, 8]} />
+          <meshBasicMaterial attach="material" color={color} />
+        </mesh>
+      </group>
+    )}
+  />
+);
+`;
+
+const CODE_BLOCK_CLASSNAME =
+  '!m-0 rb-code-block flex-1 shadow-code rounded-lg bg-[#16161E] border border-[#262631]';
 
 export interface GettingStartedProps {
   className?: string;
@@ -11,7 +45,7 @@ export interface GettingStartedProps {
 
 export const GettingStarted: FC<GettingStartedProps> = ({ className }) => {
   return (
-    <div className={cn('w-full flex flex-col', className)}>
+    <div className={cn('w-full flex flex-col gap-10', className)}>
       <Stack
         direction='column'
         className='container text-left w-full mx-auto'
@@ -24,29 +58,135 @@ export const GettingStarted: FC<GettingStartedProps> = ({ className }) => {
       <section className='flex justify-center border-t border-b border-gray-400/30 py-14'>
         <Stack
           direction='row'
-          className='container w-full'
+          className='container w-full gap-6'
           justifyContent='spaceBetween'
+          alignItems='start'
         >
-          <div>
-            <h4>Integrating Reagraph them in your application</h4>
-            <p>
-              To get started with Reagraph just need to install the package and
-              import the component.
+          <div className='flex-1'>
+            <h4 className='text-text-primary text-base font-bold md:text-xl'>
+              Integrating Reagraph them in your application
+            </h4>
+            <p className='text-text-secondary text-sm my-3 md:text-base'>
+              To get started with{' '}
+              <span className='text-cyan-300'>Reagraph</span> just need to
+              install the package and import the component.
             </p>
+            <Link
+              aria-label='Learn more about Reagraph'
+              href='/docs/getting-started/setup'
+              className='w-fit text-xs font-bold text-blue-300 hover:text-blue-400 md:text-base mt-3'
+            >
+              Learn more
+            </Link>
+          </div>
+          <Stack
+            direction='column'
+            className='flex-1 w-full'
+            alignItems='start'
+            justifyContent='start'
+          >
+            <SyntaxHighlighter
+              language='jsx'
+              style={vscDarkPlus}
+              className={CODE_BLOCK_CLASSNAME}
+            >
+              {`$ npm install reagraph -S\n \nimport { GraphCanvas } from 'reagraph';`}
+            </SyntaxHighlighter>
+          </Stack>
+        </Stack>
+      </section>
+
+      <section className='flex justify-center border-t border-b border-gray-400/30 py-14'>
+        <Stack
+          direction='row'
+          className='container w-full gap-6'
+          justifyContent='spaceBetween'
+          alignItems='start'
+        >
+          <div className='flex-1'>
+            <h4 className='text-text-primary text-base font-bold md:text-xl'>
+              Let's build something amazing
+            </h4>
+            <p className='text-text-secondary text-sm my-3 md:text-base'>
+              Let's define some nodes and edges.{' '}
+              <span className='text-cyan-300'>Nodes</span> are the blocks and{' '}
+              <span className='text-cyan-300'>edges</span> are the relationships
+              between the blocks.
+            </p>
+            <p className='text-text-secondary text-sm my-3 md:text-base'>
+              The data shapes require one property of id but you can pass label
+              or icon to them to show some sort of indication what it
+              represents.
+            </p>
+            <Link
+              aria-label='Learn more about Reagraph'
+              href='/docs/getting-started/setup'
+              className='w-fit text-xs font-bold text-blue-300 hover:text-blue-400 md:text-base mt-3'
+            >
+              Learn more
+            </Link>
           </div>
           <SyntaxHighlighter
             language='jsx'
             style={vscDarkPlus}
-            className='rb-code-block'
-            customStyle={{
-              margin: 0,
-              flex: '1 1 0%',
-              backgroundColor: 'transparent',
-              maxWidth: '90vw',
-            }}
+            className={CODE_BLOCK_CLASSNAME}
           >
-            {`$ npm install reagraph -S`}
+            {BASIC_CODE_SAMPLE}
           </SyntaxHighlighter>
+        </Stack>
+      </section>
+
+      <section className='flex justify-center border-t border-b border-gray-400/30 py-14'>
+        <Stack
+          direction='row'
+          className='container w-full gap-6'
+          justifyContent='spaceBetween'
+          alignItems='start'
+        >
+          <div className='flex-1'>
+            <h4 className='text-text-primary text-base font-bold md:text-xl'>
+              Customizing the graph
+            </h4>
+            <p className='text-text-secondary text-sm my-3 md:text-base'>
+              By default, the graph supports 2 themes:{' '}
+              <span className='text-cyan-300'>light</span> and{' '}
+              <span className='text-cyan-300'>dark</span> modes. You can also
+              define your own theme using the theme interface.
+            </p>
+            <p className='text-text-secondary text-sm my-3 md:text-base'>
+              You can also customize the graph by passing props to the
+              component.
+            </p>
+            <Link
+              aria-label='Learn more about Reagraph'
+              href='/docs/getting-started/setup'
+              className='w-fit text-xs font-bold text-blue-300 hover:text-blue-400 md:text-base mt-3'
+            >
+              Learn more
+            </Link>
+          </div>
+          <SyntaxHighlighter
+            language='jsx'
+            style={vscDarkPlus}
+            className={CODE_BLOCK_CLASSNAME}
+          >
+            {CUSTOM_THEME_CODE_SAMPLE}
+          </SyntaxHighlighter>
+        </Stack>
+      </section>
+
+      <section className='flex justify-center border-t border-b border-gray-400/30 py-14'>
+        <Stack
+          direction='column'
+          className='container w-full max-w-[600px] text-center'
+          justifyContent='spaceBetween'
+        >
+          <h2 className='text-5xl text-text-primary mb-6'>
+            Start building with Reagraph today
+          </h2>
+          <Button color='primary' size='large'>
+            Get Started
+          </Button>
         </Stack>
       </section>
     </div>
--- src/components/shared/theme-wrapper.tsx
+++ src/components/shared/theme-wrapper.tsx
@@ -3,6 +3,20 @@
 import { FC, PropsWithChildren } from 'react';
 import { ThemeProvider, theme as reablocksTheme } from 'reablocks';
 
+const theme = {
+  ...reablocksTheme,
+  components: {
+    ...reablocksTheme.components,
+    button: {
+      ...reablocksTheme.components.button,
+      sizes: {
+        ...reablocksTheme.components.button.sizes,
+        large: `${reablocksTheme.components.button.sizes.large} py-4 rounded-none`,
+      },
+    },
+  },
+};
+
 export const ThemeWrapper: FC<PropsWithChildren> = ({ children }) => {
-  return <ThemeProvider theme={reablocksTheme}>{children}</ThemeProvider>;
+  return <ThemeProvider theme={theme}>{children}</ThemeProvider>;
 };
